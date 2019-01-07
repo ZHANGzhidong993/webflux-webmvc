7f9b13a83d298772ca3d97883a85b0ca57e8f2f2
zhangzhidong <zhangzhidong@myhexin.com>
2019-01-07 19:45:54 +0800

    learning webflux

---
+++ webClient-as-caller/src/main/java/com/myhexin/webclientcaller/WebClientCallerStarter.java
@@ -0,0 +1,16 @@
+package com.myhexin.webclientcaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangzhidong
+ * @since 2019/1/7
+ */
+@SpringBootApplication
+public class WebClientCallerStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebClientCallerStarter.class);
+    }
+}
