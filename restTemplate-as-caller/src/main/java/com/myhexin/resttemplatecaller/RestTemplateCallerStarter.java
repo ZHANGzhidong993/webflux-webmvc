e59bb7285028252b1d4c5a3f73a57b9be13e2bcc
zhangzhidong <zhangzhidong@myhexin.com>
2019-01-07 17:42:50 +0800

    learning webflux

---
+++ restTemplate-as-caller/src/main/java/com/myhexin/resttemplatecaller/RestTemplateCallerStarter.java
@@ -0,0 +1,16 @@
+package com.myhexin.resttemplatecaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangzhidong
+ * @since 2019/1/7
+ */
+@SpringBootApplication
+public class RestTemplateCallerStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplateCallerStarter.class);
+    }
+}
---
+++ restTemplate-as-caller/src/main/java/com/myhexin/resttemplatecaller/controller/HelloController.java
@@ -0,0 +1,38 @@
+package com.myhexin.resttemplatecaller.controller;
+
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+/**
+ * @author zhangzhidong
+ * @since 2019/1/7
+ */
+@RestController
+public class HelloController {
+
+    private final String TARGET_HOST = "http://localhost:8092";
+    private RestTemplate restTemplate;
+
+    public HelloController() {  // 1
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setDefaultMaxPerRoute(1000);
+        connectionManager.setMaxTotal(1000);
+        this.restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory(
+                HttpClientBuilder.create().setConnectionManager(connectionManager).build()
+        ));
+
+    }
+
+    @GetMapping("/hello/{latency}")
+    public Mono<String> hello(@PathVariable int latency) {
+        return Mono.fromCallable(() -> restTemplate.getForObject(TARGET_HOST + "/hello/" + latency, String.class))
+                .subscribeOn(Schedulers.elastic());
+    }
+}
